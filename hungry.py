e7f47333fd53a780e1c1bf230c01cb9b5252a280
Sriram <sriramnaveen2416@gmail.com>
2025-03-17 11:24:11 +0530

    water

--- hungry.py
+++ hungry.py
@@ -3,6 +3,9 @@ hungry = input("Are you hungry? (yes/no): ").strip().lower()
 if hungry == "yes":
     print("Eat a samosa!")
     print("pizza")
-    p
 else:
-    print("Let it be.")
+    thirsty = input("Are you thirsty? (yes/no): ").strip().lower()  
+    if thirsty == "yes":  
+        print("Drink water")
+    else:
+        print("Let it be.")
